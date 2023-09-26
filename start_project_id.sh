d9f2319695148c8facdf1757b92dbf7e2be5c69e
schiavo.marco05@gmail.com <schiavo.marco05@gmail.com>
2023-09-26 22:35:03 +0100

    update bash start route

--- start_project_id.sh
+++ start_project_id.sh
@@ -9,11 +9,11 @@ function terminate_children {
 trap 'terminate_children; exit 2' SIGINT
 
 # Avvia il server Node.js in /home/pi/project01
-cd /home/pi/project01
+cd /home/pi/project_security/project01
 node index.js &
 
 # Avvia lo script Python in /home/pi/project02
-cd /home/pi/project02
+cd /home/pi/project_security/project02
 python main.py &
 
 # Aspetta che uno qualsiasi dei processi figli termini
